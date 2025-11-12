88f0e3b1bda037de2fc3906fa268cb50c21c062f
khoithui87 <bosskhang2000@gmail.com>
2025-11-12 19:55:34 +0700

    Cai dat trang loi 404

---
+++ src/views/NotFound.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="page">
+    <h1>404</h1>
+    <p>
+      Oops, không thể tìm thấy trang. Trở về
+      <router-link to="/">trang chủ.</router-link>
+    </p>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-size: 4rem;
+  color: #dc3545;
+}
+</style>
