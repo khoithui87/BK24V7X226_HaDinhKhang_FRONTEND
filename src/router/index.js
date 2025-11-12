efa28ad4c6fe88ec565cc621129dd7a2206689fb
khoithui87 <bosskhang2000@gmail.com>
2025-11-12 20:45:17 +0700

    Fix loi ket noi

--- src/router/index.js
+++ src/router/index.js
@@ -13,15 +13,15 @@ const routes = [
     component: () => import("@/views/NotFound.vue"),
   },
   {
+    path: "/contacts/add",
+    name: "contact.add",
+    component: () => import("@/views/ContactAdd.vue"),
+  },
+  {
     path: "/contacts/:id",
     name: "contact.edit",
     component: () => import("@/views/ContactEdit.vue"),
     props: true,
-  },
-  {
-    path: "/contacts/add",
-    name: "contact.add",
-    component: () => import("@/views/ContactAdd.vue"),
   },
 ];
 
